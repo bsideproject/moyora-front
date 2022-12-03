16e1adc062a576409117c3ba06681ec3a1740430
Kir93 <kir931028@gmail.com>
2022-12-03 17:56:32 +0900

    update react-query version 4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,8 @@ import Head from 'next/head';
 import localFont from '@next/font/local';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import theme from '@styles/theme';
 
