63cf51e8425b0ee2b44bae443445d4f6ade008f8
Kir93 <kir931028@gmail.com>
2022-12-06 17:28:01 +0900

    update moyora layout

--- components/Layout/Layout.styles.ts
+++ components/Layout/Layout.styles.ts
@@ -2,19 +2,21 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
-  min-width: 414px;
-  min-height: 100vh;
-  padding: 0 21px;
+  justify-content: center;
+  width: 390rem;
   margin: auto auto 0;
   background-color: ${({ theme }) => theme.background};
 
-  @media screen and (min-width: 768px) {
-    min-width: 768px;
-    padding: 0 22px;
+  & > main {
+    width: 390rem;
+    min-height: 100vh;
+    padding: 0 20px;
+    background-color: ${({ theme }) => theme.white};
   }
 
-  @media screen and (min-width: 1200px) {
-    max-width: 1200px;
+  @media screen and (min-width: 768px) {
+    width: 100%;
+    background-color: ${({ theme }) => theme.background};
   }
 `;
 
