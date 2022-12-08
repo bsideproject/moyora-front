5132ac97762b961efa37cf13cf7be3ef031199af
Kir93 <kir931028@gmail.com>
2022-12-08 20:32:14 +0900

    update project custom theme apply

--- theme.d.ts
+++ theme.d.ts
@@ -1,48 +1,29 @@
 import 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
-    // 가변 값
-    primaryVariant: '#0654e2';
-    primary: '#226bef';
-    primary900: '#3779f0';
-    primary800: '#4e88f2';
-    primary700: '#6397f4';
-    primary600: '#7aa6f5';
-    primary500: '#90b4f7';
-    primary400: '#a6c4f9';
-    primary300: '#bcd2fa';
-    primary200: '#d2e1fc';
-    primary100: '#e8f0fe';
-    secondaryVariant: '#9e96ff';
-    secondary: '#b296ff';
-    secondary900: '#b9a0ff';
-    secondary800: '#c1abff';
-    secondary600: '#d1c0ff';
-    secondary500: '#d8caff';
-    secondary700: '#c9b5ff';
-    secondary400: '#e0d5ff';
-    secondary200: '#f0eaff';
-    secondary300: '#e8dfff';
-    secondary100: '#f7f4ff';
-    background: '#f9fafc';
-    borderRadiusBase: '2px';
-    baseFontColor: '#000';
-    primaryButtonTextColor: '#fff';
+    primary: '#FF6D3A';
+    secondary: '#4181F0';
+    background: '#F8F9FA';
 
-    // 고정 값
-    black: '#000';
-    gray9: '#3b3f4a';
-    gray8: '#5a5e6a';
-    gray7: '#7f828c';
-    gray6: '#9da0a8';
-    gray5: '#bec1c7';
-    gray4: '#d5d8dc';
-    gray3: '#e4e6ea';
-    gray2: '#eff1f4';
-    gray1: '#f9fafc';
-    white: '#fff';
-    caution: '#f9a825';
-    error: '#e03028';
-    success: '#2fb916';
+    gray50: '#F8F9FA';
+    gray100: '#F1F3F5';
+    gray200: '#E9ECEF';
+    gray300: '#DEE2E6';
+    gray400: '#CCD2D8';
+    gray500: '#AEB5BA';
+    gray600: '#888E94';
+    gray650: '#868E96';
+    gray700: '#484F54';
+    gray750: '#3A3E43';
+    gray800: '#32363A';
+    gray850: '#26282B';
+    gray900: '#1F2022';
+    gray950: '#17181A';
+    white: '#ffffff';
+    black: '#000000';
+    negative: '#D82C0D';
+    negativeLight: '#FFF4F4';
+    positive: '#2C6ECB';
+    positiveLight: '#F2F7FE';
   }
 }
