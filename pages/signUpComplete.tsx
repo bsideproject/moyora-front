d46000f7419418aa3a9141dcd19efffa8856a36a
manbok <alsckd0195@gmail.com>
2022-12-11 22:55:14 +0900

    update variable name and initialize time

--- pages/signUpComplete.tsx
+++ pages/signUpComplete.tsx
@@ -9,47 +9,56 @@ import LinkIcon from '@public/svgs/icons/moyora-icons-link.svg';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
 
-import L from '@components/SignUp/SignUpComplete.styles';
+import S from '@components/SignUp/SignUpComplete.styles';
 
 dayjs.locale('ko');
 
 const SignUpComplete: React.FC = () => {
-  const [diffD, setDiffD] = useState('00');
-  const [diffH, setDiffH] = useState('00');
-  const [diffM, setDiffM] = useState('00');
-  const padNum = (num: number): string => {
-    return ('0' + Math.floor(num)).slice(-2);
+  const lpadZero = (num: number) => {
+    return Math.floor(num).toString().padStart(2, '0');
   };
-  const calTimer = () => {
-    const open = dayjs('2023-01-29', 'YYYY-MM-DD');
-    const now = dayjs();
-    const diff = open.diff(now, 'm');
-    const d = Math.floor(diff / 60 / 24);
-    const h = Math.floor((diff / 60) % 24);
-    const m = Math.floor(diff % 60);
-    setDiffD(padNum(d));
-    setDiffH(padNum(h));
-    setDiffM(padNum(m));
+
+  const releaseDate = dayjs('2023-01-29', 'YYYY-MM-DD');
+  let nowDate = dayjs();
+  let diffDate = releaseDate.diff(nowDate, 'm');
+  let diffDay = lpadZero(diffDate / 60 / 24);
+  let diffHour = lpadZero((diffDate / 60) % 24);
+  let diffMinute = lpadZero(diffDate % 60);
+
+  const [releaseDay, setReleaseDay] = useState(diffDay);
+  const [releaseHour, setReleaseHour] = useState(diffHour);
+  const [releaseMinute, setReleaseMinute] = useState(diffMinute);
+
+  const calculateDate = () => {
+    nowDate = dayjs();
+    diffDate = releaseDate.diff(nowDate, 'm');
+    diffDay = lpadZero(diffDate / 60 / 24);
+    diffHour = lpadZero((diffDate / 60) % 24);
+    diffMinute = lpadZero(diffDate % 60);
+
+    setReleaseDay(diffDay);
+    setReleaseHour(diffHour);
+    setReleaseMinute(diffMinute);
   };
 
   useEffect(() => {
     const timer = setInterval(() => {
-      calTimer();
+      calculateDate();
     }, 1000);
     return () => clearInterval(timer);
   }, []);
 
   return (
-    <L.SignUpCompleteWrapper>
-      <L.LogoSection>
+    <S.SignUpCompleteWrapper>
+      <S.LogoSection>
         <Image src={LogoSmall} alt="moyora-logo" />
-      </L.LogoSection>
+      </S.LogoSection>
       <div>
         <Image src={ParticleGroup} alt="moyora-particle" />
         <h3>회원가입이 완료되었어요!</h3>
-        <h2>{`${diffD}일 : ${diffH}시 : ${diffM}분`}</h2>
+        <h2>{`${releaseDay}일 : ${releaseHour}시 : ${releaseMinute}분`}</h2>
         <p>1월 29일에 정식 버전으로 만나요!</p>
-        <L.shareSection>
+        <S.shareSection>
           <p>
             더 많은 동창 친구들이 모일 수 있도록
             <br />
@@ -70,9 +79,9 @@ const SignUpComplete: React.FC = () => {
               <p>링크 복사</p>
             </div>
           </div>
-        </L.shareSection>
+        </S.shareSection>
       </div>
-    </L.SignUpCompleteWrapper>
+    </S.SignUpCompleteWrapper>
   );
 };
 
