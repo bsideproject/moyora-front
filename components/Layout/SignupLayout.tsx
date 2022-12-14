e6ce878a33805113cc52c3631484f0b16ed20047
Kir93 <kir931028@gmail.com>
2022-12-14 17:18:50 +0900

    add signupLayout

---
+++ components/Layout/SignupLayout.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Image from 'next/image';
+import { Button, Progress } from 'antd';
+
+import Moyora from '@public/svgs/logos/moyora-logo-small.svg';
+import Step1Disabled from '@public/svgs/steps/step1-disabled.svg';
+import Step2Disabled from '@public/svgs/steps/step2-disabled.svg';
+import Step3Disabled from '@public/svgs/steps/step3-disabled.svg';
+import Step4Disabled from '@public/svgs/steps/step4-disabled.svg';
+import Step1Active from '@public/svgs/steps/step1-active.svg';
+import Step2Active from '@public/svgs/steps/step2-active.svg';
+import Step3Active from '@public/svgs/steps/step3-active.svg';
+import Step4Active from '@public/svgs/steps/step4-active.svg';
+
+import L from './Layout.styles';
+
+type TStep = '1' | '2' | '3' | '4';
+
+interface IProps {
+  step: TStep;
+  children: React.ReactElement;
+}
+
+const StepContents = {
+  '1': { disabled: Step1Disabled, active: Step1Active, title: '학교 정보를 입력해 주세요.' },
+  '2': { disabled: Step2Disabled, active: Step2Active, title: '간단한 정보를 알려주세요.' },
+  '3': { disabled: Step3Disabled, active: Step3Active, title: '어떤 일을 하고 계신가요?' },
+  '4': { disabled: Step4Disabled, active: Step4Active, title: '어디에 거주하고 계시나요?' },
+};
+
+const SignupLayout: React.FC<IProps> = ({ step, children }) => {
+  return (
+    <L.SignupStepWrapper>
+      <Image src={Moyora} alt="moyora-logo" />
+      <Progress
+        showInfo={false}
+        strokeColor="#FF6D3A"
+        strokeWidth={2}
+        percent={25 * parseInt(step)}
+      />
+      <L.SignupStepWrap>
+        {Array(4)
+          .fill(null)
+          .map((_, i) => {
+            const index = (i + 1).toString() as TStep;
+            return (
+              <Image
+                key={index}
+                src={
+                  step === index.toString()
+                    ? StepContents[index].active
+                    : StepContents[index].disabled
+                }
+                alt={step === index.toString() ? 'step-active' : 'step-disabled'}
+              />
+            );
+          })}
+      </L.SignupStepWrap>
+      <h2>{StepContents[step].title}</h2>
+      <section>{children}</section>
+      <L.SignupButton type="primary" disabled>
+        다음
+      </L.SignupButton>
+    </L.SignupStepWrapper>
+  );
+};
+
+export default SignupLayout;
