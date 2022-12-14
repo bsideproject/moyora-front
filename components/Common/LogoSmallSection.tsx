f7281289d6402369fc1ba2d062f42b33efda3015
manbok <alsckd0195@gmail.com>
2022-12-14 17:04:41 +0900

    add upper common section with logo-small

---
+++ components/Common/LogoSmallSection.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+import LogoSmall from '@public/svgs/logos/moyora-logo-small.svg';
+
+import LSS from './LogoSmallSection.styles';
+
+type Props = {
+  children?: React.ReactElement;
+};
+
+const LogoSmallSection: React.FC<Props> = ({ children }) => {
+  return (
+    <>
+      <LSS>
+        <div>
+          <Image src={LogoSmall} alt="moyora-logo" />
+        </div>
+        <div>{children}</div>
+      </LSS>
+    </>
+  );
+};
+
+export default LogoSmallSection;
