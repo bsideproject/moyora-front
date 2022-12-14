b4ce3929e6e6d8f9a41d2f93be7ae74272c2f364
manbok <alsckd0195@gmail.com>
2022-12-14 20:06:15 +0900

    add upper common section with logo-small

---
+++ components/Common/LogoSmallSection.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const LSS = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 56rem;
+`;
+
+export default LSS;
