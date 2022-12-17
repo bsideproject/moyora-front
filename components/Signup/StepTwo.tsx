0e616c42d9024a5a27a22da7185564dfeaf121eb
Kir93 <kir931028@gmail.com>
2022-12-17 14:17:32 +0900

    update nickname visible changed

--- components/Signup/StepTwo.tsx
+++ components/Signup/StepTwo.tsx
@@ -1,14 +1,20 @@
+import useInput from '@utils/useInput';
 import React from 'react';
 
 import S from './Signup.styles';
 
 const StepTwo: React.FC = () => {
+  const [name, onChangeName] = useInput('');
   return (
     <S.SignupInputWrapper>
-      <h3>졸업하신 초등학교는 어디인가요?</h3>
-      <S.SignupInput placeholder="2자 이상 10자 이내" />
-      <h3>닉네임(별명)</h3>
-      <S.SignupInput placeholder="닉네임" />
+      <h3>이름</h3>
+      <S.SignupInput placeholder="2자 이상 10자 이내" value={name} onChange={onChangeName} />
+      {name ? (
+        <>
+          <h3>닉네임(별명)</h3>
+          <S.SignupInput placeholder="닉네임" />
+        </>
+      ) : null}
     </S.SignupInputWrapper>
   );
 };
