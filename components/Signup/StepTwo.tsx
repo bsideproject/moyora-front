d41278b05da6808c62b388c68bfde6e455a1db51
Kir93 <kir931028@gmail.com>
2022-12-20 13:20:52 +0900

    style

--- components/Signup/StepTwo.tsx
+++ components/Signup/StepTwo.tsx
@@ -9,10 +9,12 @@ const StepTwo: React.FC = () => {
     <S.SignupInputWrapper>
       <h3>이름</h3>
       <S.SignupInput placeholder="2자 이상 10자 이내" value={name} onChange={onChangeName} />
+      <h4>친구들이 알아볼 수 있도록 실명으로 입력해 주세요.</h4>
       {name ? (
         <>
           <h3>닉네임(별명)</h3>
           <S.SignupInput placeholder="닉네임" />
+          <h4>학창시절 불렸던 별명도 좋아요.</h4>
         </>
       ) : null}
     </S.SignupInputWrapper>
