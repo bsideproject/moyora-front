4fe112dbc2de6c539183264da2085145ea4fcbde
manbok <alsckd0195@gmail.com>
2022-12-28 15:46:08 +0900

    update logo section component n guestBok component

--- components/Common/LogoHeader/LogoHeader.tsx
+++ components/Common/LogoHeader/LogoHeader.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Image from 'next/image';
-
 import SmallLogo from '@public/svgs/moyora-logo-small.svg';
-
 import LogoHeaderWrapper from './LogoHeader.styles';
+import HaedrIcons from './HeaderIcons';
 
 interface IProps {
+  headerIcons?: boolean;
   children?: React.ReactElement;
 }
 
-const LogoHeader: React.FC<IProps> = ({ children }) => {
+const LogoHeader: React.FC<IProps> = ({ headerIcons, children }) => {
   return (
     <LogoHeaderWrapper>
       <Image src={SmallLogo} alt="moyora-logo" />
       <div>{children}</div>
+      {headerIcons ? <HaedrIcons /> : ''}
     </LogoHeaderWrapper>
   );
 };
