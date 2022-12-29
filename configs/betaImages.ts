87c8448cb3c95492b89eb507376ccd88b267ef98
Kir93 <kir931028@gmail.com>
2022-12-29 21:11:10 +0900

    add beta page images

---
+++ configs/betaImages.ts
@@ -0,0 +1,29 @@
+import MoyoraLogo from '@public/svgs/moyora-logo-small.svg';
+import BetaMain from '@public/svgs/beta-main.svg';
+import BetaHeart from '@public/svgs/beta-heart.svg';
+import BetaSchool from '@public/svgs/beta-school.svg';
+import BetaLetter from '@public/svgs/beta-letter.svg';
+import BetaChart from '@public/svgs/beta-chart.svg';
+import BetaPaper from '@public/svgs/beta-paper.svg';
+import BetaJob from '@public/svgs/beta-job.svg';
+import BetaExam from '@public/svgs/beta-exam.svg';
+import BetaExam2 from '@public/svgs/beta-exam2.svg';
+import LinkIcon from '@public/svgs/moyora-icons-link.svg';
+import KakaoIcon from '@public/svgs/kakao-icons-sharing.svg';
+
+const betaImages = {
+  MoyoraLogo,
+  BetaMain,
+  BetaHeart,
+  BetaSchool,
+  BetaLetter,
+  BetaChart,
+  BetaPaper,
+  BetaJob,
+  BetaExam,
+  BetaExam2,
+  LinkIcon,
+  KakaoIcon,
+};
+
+export default betaImages;
