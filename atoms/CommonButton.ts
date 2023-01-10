93085327dde96612bada77650db955f390b436bc
Kir93 <kir931028@gmail.com>
2023-01-10 14:27:53 +0900

    add CommonButton, TestButton atoms

---
+++ atoms/CommonButton.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { Button } from 'antd';
+
+export default styled(Button)`
+  position: absolute;
+  bottom: 32rem;
+  left: 0;
+  width: 100%;
+  height: 44rem;
+  font-size: 15rem;
+  font-weight: 600;
+  border: 0;
+`;
---
+++ atoms/TestButton.ts
@@ -0,0 +1,8 @@
+import { Button } from 'antd';
+
+import styled from 'styled-components';
+
+export default styled(Button)`
+  height: 80px;
+  margin: 30rem 0 0;
+`;
