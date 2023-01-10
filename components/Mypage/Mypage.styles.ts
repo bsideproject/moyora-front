f5298e55e715423ebfb31b197fce67dcf482938a
Kir93 <kir931028@gmail.com>
2023-01-10 14:29:32 +0900

    style

--- components/Mypage/Mypage.styles.ts
+++ components/Mypage/Mypage.styles.ts
@@ -1,5 +1,10 @@
-import Link from 'next/link';
 import styled, { createGlobalStyle } from 'styled-components';
+import Link from 'next/link';
+import { Input, Select } from 'antd';
+
+interface IInputPublicProps {
+  isFill?: boolean;
+}
 
 const GlobalStyle = createGlobalStyle`
     div.container{
@@ -119,6 +124,124 @@ const LinkCard = styled(Link)`
   }
 `;
 
-const M = { GlobalStyle, MypageIndexWrapper, ProfileWrap, LinkWrap, LinkCard };
+const MypageInputWrapper = styled.div`
+  position: relative;
+  min-height: 100vh;
+
+  & > h2 {
+    padding-top: 20rem;
+    font-size: 20rem;
+    font-weight: 700;
+    line-height: 28rem;
+    color: ${({ theme }) => theme.gray900};
+    border-top: 1rem solid ${({ theme }) => theme.gray100};
+  }
+
+  & h3 {
+    margin: 24rem 0 12rem;
+    font-size: 16rem;
+    font-weight: 500;
+    color: ${({ theme }) => theme.gray800};
+    text-align: left;
+
+    & span {
+      color: ${({ theme }) => theme.negative};
+    }
+  }
+
+  & h4 {
+    margin-top: 8rem;
+    font-size: 14rem;
+    font-weight: 400;
+    color: ${({ theme }) => theme.gray600};
+    text-align: left;
+  }
+
+  & > p {
+    margin: 72rem 0 12rem;
+    font-size: 14rem;
+    font-weight: 400;
+    line-height: 20rem;
+    color: ${({ theme }) => theme.gray600};
+    text-align: center;
+  }
+
+  & div.select-group {
+    display: flex;
+    margin: 0 -4rem;
+  }
+`;
+
+const MypageInput = styled(Input)<IInputPublicProps>`
+  padding: 9rem 10rem;
+  font-weight: 400;
+  border: 1rem solid ${({ theme, isFill }) => (isFill ? theme.gray500 : theme.gray200)};
+  border-radius: 8rem;
+
+  & + & {
+    margin-top: 12rem;
+  }
+
+  &.ant-input-affix-wrapper-readonly {
+    &:hover {
+      border: 1rem solid ${({ theme }) => theme.gray200};
+    }
+    & * {
+      cursor: pointer;
+    }
+  }
+`;
+
+const SearchButton = styled(Input.Search)<IInputPublicProps>`
+  padding: 5rem 0;
+  font-weight: 400;
+  & input {
+    cursor: pointer;
+  }
+  border: 1rem solid ${({ theme, isFill }) => (isFill ? theme.gray500 : theme.gray200)};
+  border-radius: 8rem;
+  & button {
+    border: 0;
+    & svg {
+      color: ${({ theme }) => theme.gray800};
+    }
+  }
+`;
+
+const MypageSelect = styled(Select)<IInputPublicProps>`
+  width: 100%;
+  height: 44rem;
+  text-align: left;
+
+  & div.ant-select-selector {
+    border-color: ${({ theme, isFill }) => (isFill ? theme.gray500 : theme.gray200)} !important;
+  }
+
+  &.half {
+    width: 50%;
+    margin: 0 4rem;
+  }
+
+  & > div {
+    height: 100% !important;
+    font-weight: 400;
+
+    & span {
+      margin-top: 6rem;
+    }
+  }
+`;
+
+const M = {
+  GlobalStyle,
+  MypageIndexWrapper,
+  ProfileWrap,
+  LinkWrap,
+  LinkCard,
+  MypageInputWrapper,
+  MypageInput,
+  SearchButton,
+  MypageSelect,
+};
 
 export default M;
