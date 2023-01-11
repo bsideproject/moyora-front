021e0d779f240a757e4cd2d7bbd643b4976ee9d5
Kir93 <kir931028@gmail.com>
2023-01-11 13:56:32 +0900

    style

--- components/Mypage/Mypage.styles.ts
+++ components/Mypage/Mypage.styles.ts
@@ -1,6 +1,6 @@
 import styled, { createGlobalStyle } from 'styled-components';
 import Link from 'next/link';
-import { Input, Select } from 'antd';
+import { Collapse, Drawer, Input, Modal, Select } from 'antd';
 
 interface IInputPublicProps {
   isFill?: boolean;
@@ -124,9 +124,43 @@ const LinkCard = styled(Link)`
   }
 `;
 
+const LogoutModal = styled(Modal)`
+  min-width: 350rem;
+  & div.ant-modal-content {
+    padding: 28rem;
+    & div.ant-modal-body {
+      margin-top: 32rem;
+      & button {
+        height: 44rem;
+        font-size: 15rem;
+        line-height: 22rem;
+      }
+      & button + button {
+        margin-top: 8rem;
+      }
+    }
+  }
+`;
+
 const MypageInputWrapper = styled.div`
   position: relative;
   min-height: 100vh;
+
+  & > div + div {
+    padding: 24rem 0 223rem;
+  }
+
+  & > div {
+    padding: 24rem 0;
+    border-top: 8rem solid ${({ theme }) => theme.gray50};
+
+    & > h4 {
+      font-size: 16rem;
+      font-weight: 700;
+      line-height: 24rem;
+      color: ${({ theme }) => theme.gray800};
+    }
+  }
 
   & > h2 {
     padding-top: 20rem;
@@ -135,6 +169,9 @@ const MypageInputWrapper = styled.div`
     line-height: 28rem;
     color: ${({ theme }) => theme.gray900};
     border-top: 1rem solid ${({ theme }) => theme.gray100};
+    &.custom-margin {
+      margin-bottom: 20rem;
+    }
   }
 
   & h3 {
@@ -149,14 +186,6 @@ const MypageInputWrapper = styled.div`
     }
   }
 
-  & h4 {
-    margin-top: 8rem;
-    font-size: 14rem;
-    font-weight: 400;
-    color: ${({ theme }) => theme.gray600};
-    text-align: left;
-  }
-
   & > p {
     margin: 72rem 0 12rem;
     font-size: 14rem;
@@ -169,6 +198,29 @@ const MypageInputWrapper = styled.div`
   & div.select-group {
     display: flex;
     margin: 0 -4rem;
+  }
+
+  & h5 {
+    margin-top: 16rem;
+    font-size: 14rem;
+    font-weight: 500;
+    line-height: 20rem;
+    color: ${({ theme }) => theme.primary};
+    cursor: pointer;
+  }
+
+  & span.error-text {
+    display: flex;
+    align-items: center;
+    justify-content: right;
+    margin-top: 8rem;
+    font-size: 14rem;
+    font-weight: 400;
+    line-height: 15rem;
+    color: ${({ theme }) => theme.negative};
+    & img {
+      margin-right: 9rem;
+    }
   }
 `;
 
@@ -232,16 +284,154 @@ const MypageSelect = styled(Select)<IInputPublicProps>`
   }
 `;
 
+const JobDrawer = styled(Drawer)`
+  position: absolute;
+  right: 0;
+  left: 0;
+  left: -21rem;
+  width: 390rem !important;
+  height: 80%;
+  border-radius: 24rem 24rem 0 0;
+
+  & div.ant-drawer-header {
+    border: 0;
+    & div.ant-drawer-header-title {
+      & button {
+        order: 1;
+        margin-right: 0;
+      }
+      & div {
+        margin-left: 12rem;
+      }
+    }
+  }
+`;
+
+const MypageCollapse = styled(Collapse)`
+  & div.ant-collapse-item-active {
+    & div.ant-collapse-header {
+      & span {
+        color: ${({ theme }) => theme.primary};
+      }
+    }
+  }
+  & div.ant-collapse-header {
+    padding: 15.5rem 4rem !important;
+    border-bottom: 1rem solid ${({ theme }) => theme.gray50};
+    & span {
+      font-size: 16rem;
+      font-weight: 500;
+    }
+  }
+
+  & div.ant-collapse-content-box {
+    padding: 16rem 4rem !important;
+    background-color: ${({ theme }) => theme.gray50};
+
+    & li {
+      padding: 10rem 24rem;
+      font-size: 16rem;
+      font-weight: 500;
+      cursor: pointer;
+
+      &.all {
+        color: ${({ theme }) => theme.primary};
+      }
+    }
+  }
+`;
+
+const Panel = styled(Collapse.Panel)`
+  text-align: left;
+  border-bottom: 1rem solid ${({ theme }) => theme.gray200};
+`;
+
+const MypageSwitchWrap = styled.div`
+  margin-top: 40rem;
+  & > div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 12rem;
+
+    & button {
+      min-width: 38rem;
+      height: 22rem;
+      & div.ant-switch-handle {
+        top: 3rem;
+        width: 16rem;
+        height: 16rem;
+      }
+    }
+
+    & h3 {
+      margin: 0;
+    }
+  }
+
+  & p {
+    font-size: 13rem;
+    font-weight: 400;
+    line-height: 18rem;
+
+    color: ${({ theme }) => theme.gray800};
+  }
+`;
+
+const ServiceInfoWrapper = styled.div`
+  & > div {
+    padding: 0 20rem;
+  }
+
+  & > h2 {
+    padding: 20rem 20rem 16rem;
+    font-size: 20rem;
+    font-weight: 700;
+    line-height: 28rem;
+    color: ${({ theme }) => theme.gray900};
+    border-top: 1rem solid ${({ theme }) => theme.gray100};
+    &.custom-margin {
+      margin-bottom: 20rem;
+    }
+  }
+`;
+
+const ServiceInfoTextWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16rem 20rem !important;
+  border-bottom: 1rem solid ${({ theme }) => theme.gray100};
+
+  & > p {
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 24px;
+    color: ${({ theme }) => theme.gray750};
+
+    &.primary-text {
+      color: ${({ theme }) => theme.primary};
+    }
+  }
+`;
+
 const M = {
   GlobalStyle,
   MypageIndexWrapper,
   ProfileWrap,
   LinkWrap,
   LinkCard,
+  LogoutModal,
   MypageInputWrapper,
   MypageInput,
   SearchButton,
   MypageSelect,
+  JobDrawer,
+  MypageCollapse,
+  Panel,
+  MypageSwitchWrap,
+  ServiceInfoWrapper,
+  ServiceInfoTextWrap,
 };
 
 export default M;
