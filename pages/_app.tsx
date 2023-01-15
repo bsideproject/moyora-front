6b5ce3e2629fc370c97d166d4a3168417527a8d4
Kir93 <kir931028@gmail.com>
2023-01-15 12:13:59 +0900

    fix import order

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Head from 'next/head';
 import localFont from '@next/font/local';
 import { AppProps } from 'next/app';
+import { ConfigProvider } from 'antd';
 import { ThemeProvider } from 'styled-components';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -11,7 +12,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import theme from '@styles/theme';
 
 import AppLayout from '@components/Layout/AppLayout';
-import { ConfigProvider } from 'antd';
 
 const client = new QueryClient({
   defaultOptions: {
