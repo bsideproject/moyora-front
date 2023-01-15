d570cb722a8adf7a9daae58c6e9549a915296f0f
Kir93 <kir931028@gmail.com>
2023-01-15 13:20:35 +0900

    update login api init

--- APIs/user/user.ts
+++ APIs/user/user.ts
@@ -1,9 +1,34 @@
+import { useRouter } from 'next/router';
+import { useCookies } from 'react-cookie';
 import { QueryOptions, useMutation, useQuery } from '@tanstack/react-query';
 
 import { fetch } from '@configs/axios';
-import { IUser } from './user.types';
+import { ISignin, IUser } from './user.types';
 
-export const baseUrl = '/api/user';
+export const baseUrl = '/user';
+
+export const useLogin = (options?: QueryOptions) => {
+  const router = useRouter();
+  const [, setCookie] = useCookies();
+
+  const queryKey = `${baseUrl}/signin`;
+  const queryFn = (code: string) =>
+    fetch.post(queryKey, null, { headers: { Authorization: code } }).then((res) => res.data);
+
+  const onSuccess = ({ isFirst, accessToken }: ISignin) => {
+    if (isFirst) router.push('/signup/1');
+    else {
+      setCookie('moyora', accessToken, {
+        path: '/',
+        sameSite: 'lax',
+        secure: true,
+      });
+      router.replace('/');
+    }
+  };
+
+  return useMutation([queryKey], queryFn, { onSuccess, ...options });
+};
 
 export const useGetUser = async (userId: string, options: QueryOptions) => {
   const queryKey = `${baseUrl}/user`;
--- APIs/user/user.types.ts
+++ APIs/user/user.types.ts
@@ -2,3 +2,9 @@ export interface IUser {
   id: number;
   name: string;
 }
+
+export interface ISignin {
+  accessToken: string;
+  isFirst: boolean;
+  refreshToken: string;
+}
