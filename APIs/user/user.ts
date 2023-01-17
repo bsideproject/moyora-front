6741f62dba6e3186f8966183fbf938860638c342
Kir93 <kir931028@gmail.com>
2023-01-17 14:06:54 +0900

    update login api apply

--- APIs/user/user.ts
+++ APIs/user/user.ts
@@ -4,6 +4,7 @@ import { QueryOptions, useMutation, useQuery } from '@tanstack/react-query';
 
 import { fetch } from '@configs/axios';
 import { ISignin, IUser } from './user.types';
+import { message } from 'antd';
 
 export const baseUrl = '/user';
 
@@ -14,8 +15,23 @@ export const useLogin = (options?: QueryOptions) => {
   const queryKey = `${baseUrl}/signin`;
   const queryFn = (code: string) =>
     fetch
-      .post(queryKey, null, { headers: { Authorization: 'Bearer ' + code } })
-      .then((res) => res.data);
+      .post(
+        'https://kauth.kakao.com/oauth/token',
+        {
+          grant_type: 'authorization_code',
+          client_id: process.env.NEXT_PUBLIC_KAKAO_KEY,
+          redirect_uri: 'http://localhost:3000/kakao',
+          code,
+        },
+        { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } },
+      )
+      .then(({ data }) => {
+        return fetch
+          .post(queryKey, null, {
+            headers: { Authorization: 'Bearer ' + data?.access_token },
+          })
+          .then((res) => res.data);
+      });
 
   const onSuccess = ({ isFirst, accessToken }: ISignin) => {
     if (isFirst) router.push('/signup/1');
@@ -29,7 +45,12 @@ export const useLogin = (options?: QueryOptions) => {
     }
   };
 
-  return useMutation([queryKey], queryFn, { onSuccess, ...options });
+  const onError = () =>
+    message
+      .error('로그인에 문제가 발생했습니다.\n잠시 후 다시 시도해 주세요 :(')
+      .then(() => router.replace('/'));
+
+  return useMutation([queryKey], queryFn, { onSuccess, onError, ...options });
 };
 
 export const useGetUser = async (userId: string, options: QueryOptions) => {
--- components/Layout/Layout.styles.ts
+++ components/Layout/Layout.styles.ts
@@ -14,11 +14,15 @@ const Container = styled.div`
     background-color: ${({ theme }) => theme.white};
 
     & .loading-page {
+      position: fixed;
+      top: 0;
+      left: 0;
       display: flex;
       align-items: center;
       justify-content: center;
       width: 100%;
       height: 100%;
+      background-color: ${({ theme }) => theme.background};
     }
   }
 
