01dc1350adcedac504de3b20d0e401fa42b2832c
manbok <alsckd0195@gmail.com>
2023-01-23 20:57:49 +0900

    add hambuger button at logoHeader

--- components/Common/LogoHeader/LogoHeader.tsx
+++ components/Common/LogoHeader/LogoHeader.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useToggle } from 'react-use';
 import Image from 'next/image';
 import SmallLogo from '@public/svgs/moyora-logo-small.svg';
 import SmallLogoPrimary from '@public/svgs/moyora-logo-small-primary.svg';
@@ -12,6 +13,8 @@ import ListIconPrimary from '@public/svgs/moyora-icons-list-primary.svg';
 import { useRouter } from 'next/router';
 import { Button } from 'antd';
 import Link from 'next/link';
+import CloseIcon from '@public/svgs/icon-close.svg';
+import BetaProfileImage from '@public/svgs/beta-profile.svg';
 
 interface IProps {
   headerIcons?: boolean;
@@ -21,6 +24,9 @@ interface IProps {
 
 const LogoHeader: React.FC<IProps> = ({ headerIcons, backgroundPrimary, children }) => {
   const router = useRouter();
+  const [isSelect, onToggle] = useToggle(false);
+  const [isLogout, setIsLogout] = useState(false);
+  const onClickLogout = () => router.replace('/');
   return (
     <L.LogoHeaderWrapper backgroundPrimary={backgroundPrimary}>
       <Link href="/mainPage">
@@ -43,13 +49,56 @@ const LogoHeader: React.FC<IProps> = ({ headerIcons, backgroundPrimary, children
           >
             <Image src={backgroundPrimary ? UserIconPrimary : UserIcon} alt="UserIcon" />
           </Button>
-          <Button
-            type="text"
-            shape="circle"
-            onClick={() => router.replace(`/guestBook/list`, '', { shallow: true })}
-          >
+          <Button type="text" shape="circle" onClick={onToggle}>
             <Image src={backgroundPrimary ? ListIconPrimary : ListIcon} alt="ListIcon" />
           </Button>
+
+          <L.MyDrawer
+            title={<LogoHeader />}
+            placement="left"
+            maskStyle={{ width: '100vw', marginLeft: 'calc(-50vw + 50%)' }}
+            onClose={onToggle}
+            open={isSelect}
+            closeIcon={<Image src={CloseIcon} alt="closeIcon" />}
+          >
+            <L.ProfileWrap>
+              <span>
+                <Image src={BetaProfileImage} alt="betaProfile" />
+              </span>
+              <div>
+                <div>
+                  <h2>홍길동</h2>
+                  <h3>별명</h3>
+                </div>
+                <p>모여라 초등학교 (77회 졸업)</p>
+              </div>
+            </L.ProfileWrap>
+            <Link href="/mypage">프로필</Link>
+            <span></span>
+            <Link href="/archive/myPage">내 사물함</Link>
+            <Link href="/guestBook/list/myPage">내 쪽지</Link>
+            <span></span>
+            <Link href="/statistics">우리 학교 통계</Link>
+            <Link href="/guestBook/list/mySchool">우리 학교 방명록</Link>
+            <Link href="/friends/list">동창 목록</Link>
+            <span></span>
+            <p onClick={onClickLogout}>로그아웃</p>
+            <L.LogoutModal
+              title="정말 로그아웃 하시겠어요?"
+              width={350}
+              open={isLogout}
+              closeIcon={<></>}
+              footer=""
+              centered
+            >
+              <Button block type="primary" onClick={onClickLogout}>
+                로그아웃
+              </Button>
+              <Button block onClick={() => setIsLogout(false)}>
+                취소
+              </Button>
+            </L.LogoutModal>
+          </L.MyDrawer>
         </L.HeaderIcons>
       ) : (
         ''
