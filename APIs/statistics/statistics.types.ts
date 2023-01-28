bf35535b3a6a2c22b44dc9fb43b0057f86714824
Kir93 <kir931028@gmail.com>
2023-01-28 18:26:15 +0900

    update statistics apis init

--- APIs/statistics/statistics.types.ts
+++ APIs/statistics/statistics.types.ts
@@ -3,3 +3,11 @@ export interface ICounts {
   schoolGuestBookKeyCount: number;
   userCount: number;
 }
+
+export interface IChart {
+  chart: number[];
+  data: {
+    title: 'string';
+    value: 0;
+  }[];
+}
