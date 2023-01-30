5bbc6c120a112c3f6970a67e7a90ec1570c803d5
Kir93 <kir931028@gmail.com>
2023-01-30 17:33:00 +0900

    hotfix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,18 @@
 import '@styles/global.css';
 
 import React from 'react';
-import axios from 'axios';
 import Head from 'next/head';
 import Script from 'next/script';
 import localFont from '@next/font/local';
+import { AppProps } from 'next/app';
 import { ConfigProvider } from 'antd';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { AppContext, AppProps } from 'next/app';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import { dehydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import theme from '@styles/theme';
 
 import AppLayout from '@components/Layout/AppLayout';
-
-const baseURL =
-  process.env.NODE_ENV !== 'production'
-    ? process.env.NEXT_PUBLIC_SERVER_DEV_URL
-    : process.env.NEXT_PUBLIC_SERVER_URL;
 
 const client = new QueryClient({
   defaultOptions: {
@@ -71,83 +65,59 @@ const antdTheme = {
 
 const App = ({ Component, pageProps }: AppProps) => (
   <QueryClientProvider client={client}>
-    <GlobalStyle />
-    {process.env.NODE_ENV !== 'production' ? <ReactQueryDevtools initialIsOpen={false} /> : null}
-    <ConfigProvider theme={antdTheme}>
-      <ThemeProvider theme={theme}>
-        <Head>
-          <title>모여라</title>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1, minimal-scale=1"
-          />
-          <meta name="title" content="모여라" />
-          <meta name="description" content="초등학교 온라인 동창회, 우리 학교 여기여기 모여라!" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
+    <Hydrate state={pageProps?.dehydratedState}>
+      <GlobalStyle />
+      {process.env.NODE_ENV !== 'production' ? <ReactQueryDevtools initialIsOpen={false} /> : null}
+      <ConfigProvider theme={antdTheme}>
+        <ThemeProvider theme={theme}>
+          <Head>
+            <title>모여라</title>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, maximum-scale=1, minimal-scale=1"
+            />
+            <meta name="title" content="모여라" />
+            <meta name="description" content="초등학교 온라인 동창회, 우리 학교 여기여기 모여라!" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+            <link rel="manifest" href="/icons/site.webmanifest" />
 
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://www.moyorafriends.co.kr/" />
-          <meta property="og:title" content="모여라" />
-          <meta
-            property="og:description"
-            content="초등학교 온라인 동창회, 우리 학교 여기여기 모여라!"
-          />
-          <meta property="og:image" content="https://www.moyorafriends.co.kr/thumbnail.png" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://www.moyorafriends.co.kr/" />
+            <meta property="og:title" content="모여라" />
+            <meta
+              property="og:description"
+              content="초등학교 온라인 동창회, 우리 학교 여기여기 모여라!"
+            />
+            <meta property="og:image" content="https://www.moyorafriends.co.kr/thumbnail.png" />
 
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://www.moyorafriends.co.kr/" />
-          <meta property="twitter:title" content="모여라" />
-          <meta
-            property="twitter:description"
-            content="초등학교 온라인 동창회, 우리 학교 여기여기 모여라!"
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://www.moyorafriends.co.kr/" />
+            <meta property="twitter:title" content="모여라" />
+            <meta
+              property="twitter:description"
+              content="초등학교 온라인 동창회, 우리 학교 여기여기 모여라!"
+            />
+            <meta
+              property="twitter:image"
+              content="https://www.moyorafriends.co.kr/thumbnail.png"
+            />
+          </Head>
+          <Script
+            src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js"
+            integrity="sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx"
+            crossOrigin="anonymous"
           />
-          <meta property="twitter:image" content="https://www.moyorafriends.co.kr/thumbnail.png" />
-        </Head>
-        <Script
-          src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js"
-          integrity="sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx"
-          crossOrigin="anonymous"
-        />
-        <AppLayout>
-          <main>
-            <Component {...pageProps} />
-          </main>
-        </AppLayout>
-      </ThemeProvider>
-    </ConfigProvider>
+          <AppLayout>
+            <main>
+              <Component {...pageProps} />
+            </main>
+          </AppLayout>
+        </ThemeProvider>
+      </ConfigProvider>
+    </Hydrate>
   </QueryClientProvider>
 );
 
 export default App;
-
-App.getInitialProps = async ({ ctx }: AppContext) => {
-  try {
-    const queryClient = new QueryClient();
-    const myInfo = await queryClient.getQueryData(['/user/myinfo']);
-    const moyoraCookie =
-      (ctx?.req as unknown as { cookies: { moyora: string } })?.cookies?.moyora || '';
-    if (!moyoraCookie || !myInfo) return { props: {} };
-    else {
-      const queryKey = `${baseURL}/user/myinfo`;
-      const getMyInfo = await axios
-        .get(queryKey, {
-          headers: { Authorization: `Bearer ${moyoraCookie}` },
-        })
-        .then((res) => res.data);
-      if (getMyInfo) {
-        await queryClient.setQueryData(['/user/myinfo'], getMyInfo);
-        return {
-          props: {
-            dehydratedState: dehydrate(queryClient),
-          },
-        };
-      } else throw new Error('Not Login');
-    }
-  } catch (error) {
-    console.log(error);
-    return { props: {} };
-  }
-};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import 'dayjs/locale/ko';
 
 import React, { FC } from 'react';
+import axios from 'axios';
 import dayjs from 'dayjs';
+import { GetServerSideProps } from 'next';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
 
 import Beta from '@components/Beta/Beta';
 import MainPage from '@components/MainPage/MainPage';
+import { useMyInfo } from '@APIs/user';
+
+const baseURL =
+  process.env.NODE_ENV !== 'production'
+    ? process.env.NEXT_PUBLIC_SERVER_DEV_URL
+    : process.env.NEXT_PUBLIC_SERVER_URL;
 
 const DDAY = '2023-02-08';
 const toDay = dayjs().format('YYYY-MM-DD');
@@ -12,7 +21,36 @@ const toDay = dayjs().format('YYYY-MM-DD');
 dayjs.locale('ko');
 
 const Home: FC = () => {
-  return DDAY >= toDay ? <Beta /> : <MainPage />;
+  const { data } = useMyInfo();
+  return DDAY >= toDay || (DDAY < toDay && !data) ? <Beta /> : <MainPage />;
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  try {
+    const queryClient = new QueryClient();
+    const moyoraCookie = req?.cookies?.moyora || '';
+    if (!moyoraCookie) return { props: {} };
+    else {
+      const queryKey = `${baseURL}/user/myinfo`;
+      const getMyInfo = () =>
+        axios
+          .get(queryKey, {
+            headers: { Authorization: `Bearer ${moyoraCookie}` },
+          })
+          .then((res) => res.data);
+      if (getMyInfo) {
+        await queryClient.prefetchQuery(['/user/myinfo'], getMyInfo);
+        return {
+          props: {
+            dehydratedState: dehydrate(queryClient),
+          },
+        };
+      } else throw new Error('Not Login');
+    }
+  } catch (error) {
+    console.log(error);
+    return { props: {} };
+  }
+};
