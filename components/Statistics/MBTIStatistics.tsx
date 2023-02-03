b2e0591e043d3f3f5296cdf3f82e6ad6823b243e
Kir93 <kir931028@gmail.com>
2023-02-03 23:50:59 +0900

    update 통계 api apply

--- components/Statistics/MBTIStatistics.tsx
+++ components/Statistics/MBTIStatistics.tsx
@@ -4,22 +4,54 @@ import { useToggle } from 'react-use';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, BarElement, CategoryScale, LinearScale } from 'chart.js';
 
-import { barOptions, dummyBarChartData, dummyMBTIsData } from '@configs/bigContents';
+import { barOptions } from '@configs/bigContents';
 
 import Arrow from '@public/svgs/arrow-bottom.svg';
 import S from './Statistics.styles';
+import { useMyInfo } from '@APIs/user';
+import { useGetMbti } from '@APIs/statistics';
 
 const MBTIStatistics: React.FC = () => {
   ChartJS.register(BarElement, CategoryScale, LinearScale);
+  const { data: me } = useMyInfo();
+  const { data: mbti } = useGetMbti('' + me?.schoolId, { enabled: Boolean(me) });
   const [isButtonTest, toggleButtonTest] = useToggle(true);
+  const barChartData = {
+    labels: [''],
+    datasets: [
+      {
+        data: [mbti?.chart[0]],
+        backgroundColor: '#FF6D3A',
+        borderRadius: 50,
+      },
+      {
+        data: [mbti?.chart[1] ?? 0],
+        backgroundColor: '#4181F0',
+      },
+      {
+        data: [mbti?.chart[2] ?? 0],
+        backgroundColor: '#F4B95C',
+      },
+      {
+        data: [mbti?.chart[3] ?? 0],
+        backgroundColor: '#71BA9D',
+      },
+      {
+        data: [mbti?.chart[4] ?? 0],
+        backgroundColor: '#B8C6FB',
+        borderRadius: 50,
+      },
+    ],
+  };
+  if (!mbti?.data.length) return <></>;
   return (
     <S.StatisticsCardWrap>
       <h2>내 동창들은 어떤 MBTI가 가장 많을까?</h2>
       <div className="chart-area-bar">
-        <Bar data={dummyBarChartData} options={barOptions} />
+        <Bar data={barChartData} options={barOptions} />
       </div>
       <S.StatisticsDetailWrap>
-        {dummyMBTIsData.map(({ title, value }, index) => {
+        {mbti?.data.map(({ title, value }, index) => {
           if (index > 3 && isButtonTest) return;
           return (
             <S.StatisticsDetailCard key={index.toString()} colorIndex={index}>
