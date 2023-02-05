d40e9163f93336f8eba1cdbffa5c200a0d5f51d1
Kir93 <kir931028@gmail.com>
2023-02-06 02:31:36 +0900

    hotifx

--- components/Common/GuestBookBox/GuestBookBox.styles.ts
+++ components/Common/GuestBookBox/GuestBookBox.styles.ts
@@ -50,6 +50,9 @@ const Textarea = styled.textarea<IProps>`
   outline: none;
   -webkit-box-orient: vertical;
   -webkit-line-clamp: ${({ size }) => size.line};
+  &:disabled {
+    color: ${({ theme }) => theme.gray850} !important;
+  }
 `;
 
 const LockTextArea = styled.div<IProps>`
