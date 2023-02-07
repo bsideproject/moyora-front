9ca23efb7a8ac40d5f90c1f68ee1d810b6e1f536
Kir93 <kir931028@gmail.com>
2023-02-07 11:03:20 +0900

    hotfix

--- components/Common/LogoHeader/LogoHeader.tsx
+++ components/Common/LogoHeader/LogoHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useToggle } from 'react-use';
 import Image from 'next/image';
 import SmallLogo from '@public/svgs/moyora-logo-small.svg';
